680dd1905d5cdd5e74d55242a59d81d415bdaefa
jynxio <1337816495@qq.com>
2022-09-28 23:34:10 +0800

    update

--- source/index.js
+++ source/index.js
@@ -2,4 +2,86 @@
 import "/style/reset.css";
 
 /* js */
-console.log( "success!" );
+import * as three from "three";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+
+/* shader */
+import vertex_shader from "./shader/vertex.glsl?raw";
+import fragment_shader from "./shader/fragment.glsl?raw";
+
+/* ------------------------------------------------------------------------------------------------------ */
+/* Renderer */
+const renderer = new three.WebGLRenderer( { antialias: globalThis.devicePixelRatio < 2 } );
+
+renderer.setPixelRatio( Math.min( globalThis.devicePixelRatio, 2 ) );
+renderer.setSize( globalThis.innerWidth, globalThis.innerHeight );
+
+document.body.append( renderer.domElement );
+
+/* Scene */
+const scene = new three.Scene();
+
+/* Camera */
+const camera = new three.PerspectiveCamera(
+    75,
+    globalThis.innerWidth / globalThis.innerHeight,
+    0.01,
+    100,
+);
+
+scene.add( camera );
+
+/* Controls */
+const controls = new OrbitControls( camera, renderer.domElement );
+
+controls.enableDamping = true;
+controls.target = new three.Vector3( 0, 0, 0.01 );
+
+/* Resize */
+globalThis.addEventListener( "resize", _ => {
+
+    renderer.setPixelRatio( Math.min( globalThis.devicePixelRatio, 2 ) );
+    renderer.setSize( globalThis.innerWidth, globalThis.innerHeight);
+
+    camera.aspect = globalThis.innerWidth / globalThis.innerHeight;
+    camera.updateProjectionMatrix();
+
+} );
+
+/* Render */
+renderer.setAnimationLoop( function loop() {
+
+    controls.update();
+
+    renderer.render( scene, camera );
+
+} );
+
+/* ------------------------------------------------------------------------------------------------------ */
+/* Camera */
+camera.translateZ( - 1 );
+
+/* Plane */
+const geometry = new three.PlaneGeometry( 1, 1, 50, 50 );
+
+const count = geometry.attributes.position.count;
+const randoms = new Float32Array( count );
+
+for ( let i = 0; i < count; i++ ) {
+
+    randoms[ i ] = Math.random();
+
+}
+
+geometry.setAttribute( "aRandom", new three.BufferAttribute( randoms, 1) );
+
+const material = new three.RawShaderMaterial( {
+    vertexShader: vertex_shader,
+    fragmentShader: fragment_shader,
+    side: three.DoubleSide,
+    wireframe: true,
+} );
+const mesh = new three.Mesh( geometry, material );
+
+scene.add( mesh );
+
---
+++ source/shader/vertex.glsl
@@ -0,0 +1,19 @@
+uniform mat4 projectionMatrix;
+uniform mat4 viewMatrix;
+uniform mat4 modelMatrix;
+
+attribute vec3 position;
+attribute float aRandom;
+
+void main() {
+
+    vec4 modelPosition = modelMatrix * vec4( position, 1.0 );
+
+    modelPosition.z += aRandom * 0.1;
+
+    vec4 viewPosition = viewMatrix * modelPosition;
+    vec4 projectedPosition = projectionMatrix * viewPosition;
+
+    gl_Position = projectedPosition;
+
+}
