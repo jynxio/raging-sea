bf9e1865e3f6e731c9045a35eb1205b2fdf67845
jynxio <1337816495@qq.com>
2022-10-02 23:28:35 +0800

    Update vertex.glsl

--- source/shader/vertex.glsl
+++ source/shader/vertex.glsl
@@ -1,9 +1,13 @@
-uniform float uBigWavesElevation;
+uniform float uElevation;
+uniform vec2 uFrequency;
 
 void main() {
 
     vec4 modelPosition = modelMatrix * vec4( position, 1.0 );
-    float elevation = sin( modelPosition.x ) * uBigWavesElevation;
+    float elevation =
+        sin( modelPosition.x * uFrequency.x ) *
+        sin( modelPosition.z * uFrequency.y ) *
+        uElevation;
 
     modelPosition.y += elevation;
 
