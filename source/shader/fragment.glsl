7bdcbf22908e66b7ef28aa93a032d0ba4f2027b9
jynxio <1337816495@qq.com>
2022-10-02 23:59:08 +0800

    Update fragment.glsl

--- source/shader/fragment.glsl
+++ source/shader/fragment.glsl
@@ -1,5 +1,15 @@
+uniform vec3 uDepthColor;
+uniform vec3 uSurfaceColor;
+uniform float uColorOffset;
+uniform float uColorMultiplier;
+
+varying float vElevation;
+
 void main() {
 
-    gl_FragColor = vec4( 0.5, 0.8, 1.0, 1.0 );
+    float strength = ( vElevation + uColorOffset ) * uColorMultiplier;
+    vec3 color = mix( uDepthColor, uSurfaceColor, strength );
+
+    gl_FragColor = vec4( color, 1.0 );
 
 }
